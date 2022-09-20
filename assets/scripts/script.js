4c92aecadb5edba84bb82c4b51af14b0b7e951f7
wlk <william.koelling@gmail.com>
2022-09-20 12:02:06 -0500

    added event listners + callbacks

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -26,10 +26,12 @@ var timer = {
     
 }
 
-var question = {
+var quiz = {
     questionElem : document.querySelector(".question"),
     listElem : document.querySelector("ol"),
     listChars : "abcdefghijklmnopqrstuvwxyz",
+
+    solutionIndex : 0,
 
     displayQuestion : function (question="Question?") {
         this.questionElem.textContent = question;
@@ -39,11 +41,32 @@ var question = {
         removeChildElements(this.listElem);
         for(var i = 0; i < answers.length; i++) {
             var newElement = document.createElement("button");
-            newElement.textContent = `${this.listChars.charAt(i)}. ${answers[i]}`;
-            newElement.setAttribute("id", `ans-${i}`);
-            this.listElem.appendChild( newElement );
+            newElement.textContent = `${this.listChars.charAt(i)}. ${answers[i]}`; // prepends : a,b,c,d, etc to the answer
+            newElement.setAttribute("id", `ans-${i}`); // adds an indexed id to each element 
+            this.listElem.appendChild( newElement ); // adds new element to the parent element
         }
     },
+
+    addListeners : function (total) {
+        for (var i = 0; i < total; i++) {
+            var elem = document.getElementById(`ans-${i}`); // get each answer elem by the indexed id
+            elem.solutionIndex = this.solutionIndex
+            console.log(this.solutionIndex)
+            elem.addEventListener("click", this.evaluateAnswer)
+        }
+    },
+
+    evaluateAnswer : function (evt) {
+        if (evt.currentTarget.id === String(`ans-${evt.currentTarget.solutionIndex}`)){
+            console.log("Correct Answer");
+        } else {
+            console.log("Incorrect Answer");
+        }
+
+        // nextQuestionCallback()
+    },
+
+
 }
 
 function removeChildElements (element) {
@@ -64,5 +87,6 @@ function toggleElementDisplay(element, preferredDisplay="block") {
 }
 
 toggleElementDisplay(document.querySelector(".quiz-prompt"));
-question.displayQuestion();
-question.displayAnswers();
+quiz.displayQuestion();
+quiz.displayAnswers();
+quiz.addListeners(4, 0);

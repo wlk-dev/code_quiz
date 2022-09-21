1afbb8527560e26e6feb6c7eb39602b37d1af3ba
wlk <william.koelling@gmail.com>
2022-09-21 18:39:46 -0500

    started last refactor

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,380 +1,189 @@
-var timer = {
-    element : document.getElementById("quiz-timer"),
+var PageState = {
+    quizPrompt : {elem : $(".quiz-prompt"), pref : "flex"},
+    quizSection : {elem : $(".quiz-section"), pref : "block"},
+    postQuiz : {elem : $(".post-quiz"), pref : "flex"},
+    scoreBoard : {elem : $(".quiz-scoreboard"), pref : "block"},
+
+    getAllElements : function () {
+        return [ this.quizPrompt, this.quizSection, this.postQuiz, this.scoreBoard ]
+    },
+
+    hideAll : function () {
+        var all = this.getAllElements()
+        for ( const idx in all ) {
+            var obj = all[idx];
+            if ( this.isVisible( obj.elem ) ) {
+                this.hideElement(obj.elem, obj.pref);
+            }
+        }
+    },
+
+    hideElement : function (element) {
+        element.css('display', "none");
+    },
+
+    showElement : function ( element, preferred="block" ) {
+        console.log(element, preferred);
+        element.css('display', preferred);
+    },
+
+    toggleElement : function ( element, preferred ) {
+        if ( !this.isVisible(element) ) {
+            element.css('display', preferred);
+        } else {
+            element.css('display', "none");
+        }
+    },
+
+    isVisible : function (element) {
+        return (element.css('display') === 'none') ? false : true;
+    },
+
+    showState : function ( stateObj ) {
+       PageState.hideAll()
+       PageState.showElement(stateObj.elem, stateObj.pref);
+    },
+} 
+
+var BoardStorage = {
+    initialized : false,
+
+    init : function () {
+        if (!localStorage.getItem("qsb")) {
+            localStorage.setItem("qsb", "{}")
+        }
+        this.initialized = true;
+    },
+
+    setScores : function ( scoreBoard ) {
+        localStorage.setItem("qsb", JSON.stringify(scoreBoard))
+    },
+
+    getScores : function () {
+        if (!this.initialized) {
+            console.warn("Trying to retrieve scores without initializing storage.")
+        }
+
+        return JSON.parse(localStorage.getItem("qsb"))
+    },
+}
+
+var Timer = {
     timerInterval : undefined,
     timeLeft : 60,
     timePenalty : 10,
-    visible : true,
 
     updateText : function () {
-        this.element.textContent = `Time left: ${this.timeLeft}`
-    },
-
-    toggleVisible : function () {
-        this.visible = toggleElementDisplay(this.element);
+        $("#quiz-timer").text(String(`Time left: ${this.timeLeft}`));
     },
 
     resetTimer : function () {
         this.timeLeft = 60;
         this.updateText();
-        this.stopTimer()
-    },
-    
-    invokePenalty : function () {
-        this.timeLeft -= this.timePenalty;
-        this.updateText();
+        this.stopTimer();
     },
 
-    tickTime : function () {
-        this.timeLeft--;
-        this.updateText();
-    },
-    
     stopTimer : function () {
         clearInterval(this.timerInterval);
-    },
+    }, 
 
-    startTimer : function () {
-        var self = this
+    startTimer : function ( onEndCallback ) {
+        var self = this;
         this.timerInterval = setInterval( function () {
-            self.tickTime();
-            if (self.timeLeft === 0) {
-                self.stopTimer()
-                quizFailed();
+            if ( self.timeLeft > 0 ) {
+                self.timeLeft--;
+                self.updateText();
+            } else {
+                self.resetTimer();
+                onEndCallback();
             }
 
-        }, 1000)
+        }, 1000 )
     }
 }
 
-var questionBank = {
-    questions : [],
-    currentQuestion : 0,
+var ScoreBoard = {
+    table : {},
+
+    loadTable : function (newTable) {
+        this.table = newTable;
+    },
+
+    addEntry : function (name, score) {
+        this.table[name] = score;
+    },
+
+    deleteEntry : function ( name ) {
+        if (this.table[name]) {
+            delete this.table[name]
+        }
+    },
+
+    getScoreStrings : function () {
+        var scoreStrings = [];
+        for ( const key in this.table ) {
+            scoreStrings.push( `${key} : ${this.table[key]}` )
+        }
+        return scoreStrings;
+    },
+
+    showScores : function () {
+        var arr = this.getScoreStrings()
+        if ( arr.length ) { // check if array is empty
+            for ( const idx in arr ) {
+                $("#score-table").append( `<button>${arr[idx]}</button>` );
+            }
+        }   
+    }
+}
+
+var QuestionBank = {
+    questionObjs : [],
     listChars : "abcdefghijklmnopqrstuvwxyz",
 
-    addQuestion : function (questionText, responses, answerIndex) {
-        var questionObj = { text : questionText, responses : [] }
-        var list = createListElements(responses.length, answerIndex);
-        for ( var i = 0; i < responses.length; i++) {
-            list[i].textContent = String(`${this.listChars.charAt(i)}. ${responses[i]}`); // prepend a char to explicitly indicate the different options
-            questionObj.responses.push( list[i] );
+    addQuestion : function( text, responses, index ) { // question text, possible responses, index of the correct response
+        var listElems = [];
+        for ( const idx in responses ) {
+            if (idx === index) {
+                listElems.push( $(`<button class="data-ans">${this.listChars[idx]}. ${responses[idx]}</button>`) )
+            } else {
+                listElems.push( $(`<button>${this.listChars[idx]}. ${responses[idx]}</button>`) )
+            }
         }
-        this.questions.push( questionObj );
+
+        this.questionObjs.push( { text: text, responses : listElems } )
     },
 
     totalQuestions : function () { 
         return this.questions.length; // we don't want to store this in memory we want to be sure we are getting an up to date value
     },
-
-    hasQuestions : function () {
-        if (!!this.questions.length) {
-            return true;
-        } else {
-            console.warn("User is most likely trying to retrieve a question, however none exist.") 
-            return false; // in our use case this should never return 'false' so if that happened something is likely wrong
-        }
-    },
-
-    getCurrentQuestion : function () {
-        if ( this.hasQuestions() ) { // if we have questions, return the current one
-            return this.questions[this.currentQuestion];
-        }
-    },
-
-    nextQuestion : function () {
-        if ( this.currentQuestion < this.totalQuestions() ) {
-            this.currentQuestion++;
-            return this.getCurrentQuestion();
-        }
-        return null;
-    },
-
-    previousQuestion : function () {
-        if ( this.currentQuestion > 0 ) {
-            this.currentQuestion--;
-            return this.getCurrentQuestion();
-        }
-        return null;
-    },
-
-    setQuestion : function (index) {
-        if ( index > this.totalQuestions() || index < 0 ) {
-            console.warn(`${index} is not a valid question index, valid range: 0 .. ${this.totalQuestions()-1}`)
-            return;
-        }
-        this.currentQuestion = index;
-        return this.getCurrentQuestion();
-    }
 }
 
-var scoreBoard = {
-    aware : false,
-    currentIndex : 0,
-
-    currentScore : 0,
-
-    submitPrompt : document.querySelector(".post-quiz"), // submit score screen
-    submitBtn : document.querySelector("#submit-result"),
-    scoreBoard : document.querySelector(".quiz-scoreboard"),
-    scoreTable : document.querySelector("#score-table"),
-
-    promptVisible : true,
-    scoreBoardVisible : true,
-
-    init : function () { // in hindsight, i could probably store all scores in a single key, value pair
-        var result;
-        if ( !!( result = localStorage.getItem("qsb-tr-idx") ) ) {
-            this.currentIndex = result;
-            this.aware = true
-        } else {
-            localStorage.setItem("qsb-tr-idx", 0);
-            this.aware = true
-        }
-    },
-
-    updateTracking : function () {
-        if (!this.aware) {
-            console.warn("Please init the scoreboard before trying to use it.")
-            return;
-        }
-        this.currentIndex++;
-        localStorage.setItem("qsb-tr-idx", this.currentIndex);
-    },
-
-    addScore : function (nickname, score) {
-        if (!this.aware) {
-            console.warn("Please init the scoreboard before trying to use it.")
-            return;
-        }
-
-        // use JSON.stringify
-        nickname = nickname.replaceAll(":", "-"); // : is used to separate data, so we dont want that being in the nickname
-        if ( this.currentScore === "failed" ) {
-            localStorage.setItem(String(`qsb-sc-${this.currentIndex}`), String(`${nickname}:failed`))
-        } else {
-            localStorage.setItem(String(`qsb-sc-${this.currentIndex}`), String(`${nickname}:${score}`))
-        }
-        this.updateTracking();
-    },
-
-    getScores : function () {
-        if (!this.aware) {
-            console.warn("Please init the scoreboard before trying to use it.")
-            return;
-        }
-        var scores = [];
-        for ( var i = 0; i < this.currentIndex; i++ ) {
-            var item = localStorage.getItem( `qsb-sc-${i}` ).split(":");
-            if ( item ) {
-                scores.push( { name : item[0], score : item[1] } )
-            }
-        }
-        return scores;
-    },
-
-    fillScoreTable : function () {
-        removeChildElements( this.scoreTable )
-        var scores = this.getScores()
-        var elements = createListElements(scores.length, 0);
-        for ( const idx in  elements) {
-            elements[idx].textContent = String(`${scores[idx].name} : ${scores[idx].score}`);
-            this.scoreTable.appendChild(elements[idx]);
-        }
-    },
-
-    clearScoreBoardData : function () { // clears all data related to the scoreboard from localStorage
-        for ( const prop in localStorage ) {
-            if ( prop.includes("qsb-sc-") || prop === "qsb-tr-idx" ) {
-                localStorage.removeItem(prop);
-            }
-        }
-        this.currentIndex = 0;
-
-        scoreBoard.init() // idk why this.init doesnt work
-    },
-
-    togglePromptVisible : function () {
-        $("#final-score").text( String(`${this.currentScore}/50`) );
-        this.promptVisible = toggleElementDisplay(this.submitPrompt, "flex");
-    },
-
-    toggleScoreBoardVisible : function () {
-        scoreBoard.fillScoreTable();
-        this.scoreBoardVisible = toggleElementDisplay(this.scoreBoard)
-    },
-
-}
-
-var quizState = {
-    promptElem : document.querySelector(".quiz-prompt"),    // initial prompt to start quiz
-    startElem : document.getElementById("start-btn"),       // start quiz button
-    quizElem : document.querySelector(".quiz-section"),     // containing div
-    questionElem : document.querySelector(".question"),     // question h2 tag, this shows the actual question that is being asked
-    listElem : document.getElementById("responses"),        // this is the where all the response will appear
-    resultElem : document.getElementById("result"),         // this rests below all the other elements, this is where we say, 'Correct!' 'Wrong!'
-
-    quizVisible : false,
-    promptVisible : true,
-
+var Quiz = {
     
-    toggleQuizVisible : function () {
-        this.quizVisible = toggleElementDisplay(this.quizElem);
-        return this.quizVisible;
-    },
-
-    togglePromptVisible : function () {
-        this.promptVisible = toggleElementDisplay(this.promptElem, "flex");
-    },
-
-    showQuestion : function ( question ) {
-        this.questionElem.textContent = question;
-    },
-
-    showResponses : function ( resps ) {
-        removeChildElements( this.listElem )
-        for ( const idx in resps ) {
-            this.listElem.appendChild(resps[idx]);
-        }
-    },
-
-    presentQuestion : function (question) {
-        this.showQuestion( question.text );
-        this.showResponses( question.responses )
-    }
-
 }
 
-quizState.listElem.addEventListener("click", function (event) {
-    if (event.target.matches("button")) { // this works you tested it in console
-        if ( event.target.dataset.ans ) {
-            console.log("Correct Answer!")
-            quizState.resultElem.textContent = "Correct!"
-            scoreBoard.currentScore += 10
-        } else {
-            console.log("Wrong Answer!")
-            quizState.resultElem.textContent = "Wrong!" // probably move this shit into a function, and have a fade out (do fade out stuff last, seems a bit time consuming)
-            timer.invokePenalty();
-        }
-
-        var ques;
-        if ( (ques = questionBank.nextQuestion()) ) {
-            quizState.presentQuestion(ques);
-        } else {
-            timer.stopTimer();
-            quizState.toggleQuizVisible();
-            scoreBoard.togglePromptVisible();
-            console.log("Quiz over!")
-        }
-
-    }
+$("#score-link").click( function (evt) {
+    PageState.showState( PageState.scoreBoard);
 });
 
-quizState.startElem.addEventListener("click", function () {
-    quizState.togglePromptVisible();
-    quizState.toggleQuizVisible();
-    scoreBoard.currentScore = 0;
-    timer.resetTimer();
-    timer.startTimer();
+$("#home-link").click( function (evt) {
+    PageState.showState( PageState.quizPrompt );
 });
 
-
-scoreBoard.submitBtn.addEventListener("click", function () {
-    var initials = $("#initials").val();
-    if ( !initials ) {
-        initials = "anon"
-    }
-
-    scoreBoard.addScore(initials, String(`${scoreBoard.currentScore}/50`))
-    scoreBoard.togglePromptVisible();
-    scoreBoard.toggleScoreBoardVisible();
-
-});
-
-$("#reset-score").click( function () {
-    scoreBoard.clearScoreBoardData();
-    scoreBoard.fillScoreTable();
-});
-
-$("#score-link").click( function () {
-    if ( quizState.promptVisible ) {
-        quizState.togglePromptVisible();
-    } else if ( quizState.quizVisible ) {
-        quizState.toggleQuizVisible();
-    }
-
-    generalStateReset()
-    scoreBoard.toggleScoreBoardVisible();
-});
-
-$("#home-link").click( function () {
-    if ( scoreBoard.scoreBoardVisible ) {
-        scoreBoard.toggleScoreBoardVisible()
-    } else if (scoreBoard.promptVisible) {
-        scoreBoard.togglePromptVisible();
-    } else if ( quizState.quizVisible ) {
-        quizState.toggleQuizVisible();
-    }
-
-    generalStateReset()
-    quizState.togglePromptVisible();
-});
-
-
-function generalStateReset () {
-    timer.resetTimer();
-    questionBank.setQuestion(0);
-    scoreBoard.currentScore = 0;
-    quizState.resultElem.textContent = ""
-}
-
-function quizFailed () {
-    timer.stopTimer();
-    questionBank.setQuestion(0);
-    quizState.toggleQuizVisible();
-    scoreBoard.togglePromptVisible();
-    scoreBoard.currentScore = "failed";
-}
-
-function createListElements(length, ansIndex) { // this works as well
-    var list = [];
-    for( var i = 0; i < length; i++ ) {
-        list.push( document.createElement("button") )
-        if (i == ansIndex) {
-            list[i].setAttribute("data-ans", "true");
-        }
-    }
-    return list;
-}
-
-function removeChildElements (element) {            // clears all children from an element
-    while (element.firstChild) {                    // while firstChild exists
-        element.removeChild(element.firstChild);    // remove firstChild
-    } 
-}
-
-function toggleElementDisplay(element, preferredDisplay="block") {
-    if (element.style.display === "none") {
-        element.style.display = preferredDisplay;
-        return true;
-    } else {
-        element.style.display = "none";
-        return false;
-    }
-}
+// $("#submit-result").click( function(evt) {
+//     scoreBoard.addEntry( $("#initials").val(), score)
+//     BoardStorage.setScores(scoreBoard.table);
+//     pageState.showState( pageState.scoreBoard );
+// });
 
 function main () {
-    scoreBoard.init()
-    // quizState.togglePromptVisible();
-    quizState.toggleQuizVisible();
-    scoreBoard.togglePromptVisible();
-    scoreBoard.toggleScoreBoardVisible();
-    
-    questionBank.addQuestion( "Commonly used data types DO NOT include:", ["strings", "booleans", "alerts", "numbers"], 2 );
-    questionBank.addQuestion( "The condition in an if / else statement is enclosed with ____.", ["quotes", "curly brackets", "parentheses", "square brackets"], 2 );
-    questionBank.addQuestion( "Arrays in JavaScript can be used to store ____.", ["numbers and strings", "other arrays", "booleans", "all of the above"], 3 );
-    questionBank.addQuestion( "String values must be enclosed within ____ when being assigned to variable.", ["commas", "curly brackets", "quotes", "parentheses"], 2 );
-    questionBank.addQuestion( "A very useful tool used during development and debugging for printing content to the debugger is:", ["JavaScript", "terminal / bash", "for loops", "console.log"], 3 );
-    quizState.presentQuestion( questionBank.getCurrentQuestion() )  
-    
+    BoardStorage.init();
+    ScoreBoard.loadTable(BoardStorage.getScores());
+    ScoreBoard.showScores()
+
+    PageState.hideAll();
+    PageState.showState(PageState.quizPrompt);
 }
 
 main();
